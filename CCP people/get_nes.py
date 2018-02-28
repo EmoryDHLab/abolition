da5d668d54545fe4aa84b0669da6f547c6838763
Arshi School <asingh361@gatech.edu>
2018-02-27 22:53:56 -0500

    simplified counting names and improved title case

--- CCP people/get_nes.py
+++ CCP people/get_nes.py
@@ -27,7 +27,7 @@ def get_entities(tagger_output):
     if current_entity:
         ents.append(current_entity)
 
-    return ['%s_%s' % (' '.join([tok for tok, tag in entity]), entity[0][1]) for entity in ents]
+    return ['%s_%s' % ((' '.join([tok for tok, tag in entity])).title(), entity[0][1]) for entity in ents]
 
 
 # open a new csv file and write down the data
@@ -42,21 +42,16 @@ with open('ccp_data.csv', 'w', newline='') as csvfile:
         year = filename.split(".")[0]
         place = filename.split(".")[1].split("-")[0] # e.g. NY-10, take NY
         with open(f, encoding='utf-8') as fin:
-            entities = []
+            entities = Counter()
             for line in fin:
                 if line.split():
                     try:
                         tagged_line = tagger.tag(line.split())
-                        entities += get_entities(tagged_line)
+                        entities += Counter(get_entities(tagged_line))
                     except:
                         pass
 
-            entity_tuples = [entity.split('_') for entity in entities]
-            for item in entity_tuples:
-                name = item[0].title() # name
-                counter[name] += 1
-
-        for name in counter:
+        for name in counter.keys():
             # store names into new file
             newfile.writerow({'Person': name, 'Count': str(counter[name]), 'Year': year, 'Place': place,
                               'filename': filename})
