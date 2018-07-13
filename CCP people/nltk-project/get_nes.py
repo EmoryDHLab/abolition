0b30e681d1aa9c8ed8be79053432b3cecb325108
Arshi School <asingh361@gatech.edu>
2018-07-12 20:20:23 -0400

    edit get_nes file

--- CCP people/nltk-project/get_nes.py
+++ CCP people/nltk-project/get_nes.py
@@ -8,14 +8,9 @@ from nltk.parse.corenlp import CoreNLPParser, CoreNLPServer
 # columns: Name, Year, Place, filename
 jar = 'stanford-corenlp-full-2018-02-27/stanford-corenlp-3.9.1.jar'
 models = 'stanford-corenlp-full-2018-02-27/stanford-corenlp-3.9.1-models.jar'
-
-# you can also access the parser by running this on your terminal:
-# java -mx4g -cp "stanford-corenlp-full-2018-02-27/*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer
-# and then accessing this on your web browser:
-# https://localhost:9000/
-
 with CoreNLPServer(path_to_jar=jar, path_to_models_jar=models, java_options=['-mx4g']) as server:
     tagger = CoreNLPParser(tagtype='ner')
+    print("server is ready, go to http://localhost:9000 to check out the annotator!")
 
     # get all the names and its attached tags
     def get_entities(tagger_output):
@@ -33,24 +28,22 @@ with CoreNLPServer(path_to_jar=jar, path_to_models_jar=models, java_options=['-m
 
         return ['%s_%s' % ((' '.join([tok for tok, tag in entity])).title(), entity[0][1]) for entity in ents]
 
-
     # open a new csv file and write down the data
     with open('ccp_data.csv', 'w', newline='') as csvfile:
         newfile = csv.DictWriter(csvfile, fieldnames=['Person', 'Count', 'Year', 'Place', 'filename'],
                                  quoting=csv.QUOTE_MINIMAL)
         newfile.writeheader()
-        entities = Counter()
-        for f in glob(os.path.join("ccpminutes", '*txt')):
+        for f in glob(os.path.join("ccpminutes", '*.txt')):
             # collect metadata
             filename = f.split("\\")[1]
             year = filename.split(".")[0]
             place = filename.split(".")[1].split("-")[0] # e.g. NY-10, take NY
+            entities = Counter()
             with open(f, encoding='utf-8') as fin:
                 for line in fin:
                     if line.split():
                         try:
                             tagged_line = tagger.tag(line.split())
-                            # print(tagged_line)
                             entities += Counter(get_entities(tagged_line))
                         except:
                             pass
