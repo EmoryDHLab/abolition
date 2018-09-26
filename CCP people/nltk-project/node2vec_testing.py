28ef05c36361ce00cf482d25f635899a82deafe3
Arshi School <asingh361@gatech.edu>
2018-09-26 15:11:58 -0400

    add node2vec_testing.py

---
+++ CCP people/nltk-project/node2vec_testing.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import node2vec
+from sklearn.manifold import TSNE
+
+# Node2Vec
+
+G = nx.read_gml('../data/co_occurrence_graph.gml')
+# use this dictionary to change any argument value!
+print("Loaded graph")
+n2v_args = {"dimensions": 128, "walk_length": 10, "num_walks": 10, "p": 1, "q": 0.5, "workers": 4}
+node2vec = node2vec.Node2Vec(G, **n2v_args)
+print("model created")
+model = node2vec.fit(window=10, min_count=1, batch_words=4)
+print("model training complete")
+print(model.wv.most_similar('Cornish'))
+model.wv.save_word2vec_format('../data/co_graph.emb')
+model.save('../data/co_graph.model')
+print("formats saved")
+
+labels = []
+tokens = []
+
+for word in model.wv.vocab:
+    tokens.append(model.wv[word])
+    labels.append(word)
+
+tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
+print("viz model created")
+new_values = tsne_model.fit_transform(tokens)
+print("viz model reducing dimensions")
+
+x = []
+y = []
+for value in new_values:
+    x.append(value[0])
+    y.append(value[1])
+
+plt.figure(figsize=(16, 16))
+
+plt.savefig('../data/node2vec_communities.png')
+print("node2vec visualization finished and saved")
